d99b0cedf6f9efbbb5151cb6c08f5ee16d981f86
lgcarrilloc <mgoluiswar@hotmail.com>
2026-04-26 23:01:54 -0500

    mejora de interfaz, tablas de multiplicar generales y mas

--- multiplicar.js
+++ multiplicar.js
@@ -1,8 +1,9 @@
 function generarTablas() {
     let contenedor = document.getElementById("contenedor-tabla");
+    let numero = document.getElementById("numero-tabla").value;
     let contenido = "";
-    for (let i = 1; i <= 10; i++) {
-        contenido += "<div>3 x " + i + " = " + (3 * i) + "</div>";
+    for (let i = 1; i <= 15; i++) {
+        contenido += "<div>" + numero + "x" + i + " = " + (numero * i) + "</div>";
     }
     contenedor.innerHTML = contenido;
 }
